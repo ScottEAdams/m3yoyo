84482727f680fafc03b06e53bb6f8f5773ba2bee
Scott Adams <scottadams80@gmail.com>
2020-04-03 01:12:18 +0200

    Updates

---
+++ precache-manifest.70a4ffed412fe946241f5dde8af228d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3c0c52297a950e0f38cd8a835328a7",
+    "url": "/m3yoyo/index.html"
+  },
+  {
+    "revision": "f492c7fc275dc54fb19a",
+    "url": "/m3yoyo/static/css/main.d97846bb.chunk.css"
+  },
+  {
+    "revision": "89ed85960f60072cb0e1",
+    "url": "/m3yoyo/static/js/2.72e2d7d5.chunk.js"
+  },
+  {
+    "revision": "b980a6607bae2171b6f1d2846f8c0b79",
+    "url": "/m3yoyo/static/js/2.72e2d7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f492c7fc275dc54fb19a",
+    "url": "/m3yoyo/static/js/main.d681cea2.chunk.js"
+  },
+  {
+    "revision": "5e3e85da79714945161a",
+    "url": "/m3yoyo/static/js/runtime-main.66e8958b.js"
+  }
+]);
